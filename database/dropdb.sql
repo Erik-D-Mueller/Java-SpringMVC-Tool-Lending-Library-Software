272770b24f6975fabe3e01c3035f002a56b5d2e5
David Wintrich <david@techelevator.com>
2016-04-18 06:35:51 -0400

    removed toollib references from database scripts

--- database/dropdb.sql
+++ database/dropdb.sql
@@ -1,8 +1,8 @@
 -- **************************************************************
--- This script destroys the toollib database and associated users
+-- This script destroys the database and associated users
 -- **************************************************************
 
--- The following line terminates any active connections to the toollib database so that it can be destroyed
+-- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'capstone';
 
 DROP DATABASE capstone;
--- database/schema.sql
+++ database/schema.sql
@@ -1,29 +1,9 @@
 -- *************************************************************************************************
--- This script creates all of the database objects (tables, sequences, etc) for the toollib database
+-- This script creates all of the database objects (tables, sequences, etc) for the database
 -- *************************************************************************************************
 
 BEGIN;
 
-CREATE TABLE tool_category (
-    tool_category_id integer primary key,
-    name varchar(64) not null
-);
-
-CREATE TABLE tool (
-    tool_id integer primary key,
-    tool_category_id integer not null references tool_category(tool_category_id),
-    name varchar(64) not null,
-    description text,
-    loan_period_in_days integer not null
-);
-
-CREATE SEQUENCE seq_tool_id;
-
-CREATE TABLE tool_inventory (
-    tool_inventory_id integer primary key,
-    tool_id integer not null references tool(tool_id)
-);
-
-CREATE SEQUENCE seq_tool_inventory_id;
+-- CREATE statements go here
 
 COMMIT;
