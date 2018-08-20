d052d8c8bfa31c077c745ff983513031a968a596
Kyle Vasquez <vasqueka@gmail.com>
2018-08-20 12:57:34 -0400

    added test users with new passwords and salt

--- database/data.sql
+++ database/data.sql
@@ -13,6 +13,11 @@ INSERT INTO app_user (user_name, password, role, drivers_license, salt) VALUES (
 INSERT INTO app_user (user_name, password, role, salt) VALUES ('BELLA', 'bella', 'LIBRARIAN', 'bella');
 INSERT INTO app_user (user_name, password, role, drivers_license, salt) VALUES ('KYLE', 'kyle', 'MEMBER', 'FF123456', 'kyle');
 INSERT INTO app_user (user_name, password, role, drivers_license, salt) VALUES ('ANDREW', 'andrew', 'MEMBER', 'GG123456', 'andrew');
+INSERT INTO app_user (user_name, password, role, drivers_license, salt) VALUES ('TEST USER', '4DfsV6VjDnPdWCOiYs9Dww==', 'MEMBER', 'TT123456', 'ehwrDx5gJ/liDjAkY5AG1mR+ZO2/9zEtVUym8Im/TIx+Db7WoYIjVLPsQw8ACooz3xONYCRaVlTyAa9ZNGpSEgsfcFFfK4tcHuVtAH0ZHRvBaPPX44b6doWnnGeLx7jTulwrC8jagnD/h/L2yMYMdmYHwAKEv04wR/kQ0C90Eoo=
+');
+INSERT INTO app_user (user_name, password, role, drivers_license, salt) VALUES ('TEST LIBRARIAN', 'RnQc58wwMDxPzRlWJSoFcw==', 'LIBRARIAN', 'LL123456', 'gypTa8CG/IReW9zAIXk0KH84NbBZtPxvCmB7B4nViQRpXiLPl4D7UKx0ygZ2rB7YtRsoePKj426DtiRM+VnQ8bZjQKhw+QDFTp3oQZOm4qUqmDriy8QkMecKN1i3DYWH5YMOS6FCRVRZBlNk6Tf4RJ3LAy9aY/2ztVVVrkCB8fQ=
+');
+
 
 INSERT INTO tool_type (tool_name, tool_description) VALUES ('HAMMER', '16 oz Fiber Glass Claw Hammer Heavy Duty Construction Shock Resistant Rubberized Non Slip Handle');
 INSERT INTO tool_type (tool_name, tool_description) VALUES ('SAW', 'Stanley 20-045 15-Inch Fat Max Hand Saw');
