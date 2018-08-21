633ae7a85dca892c11bc430ab94c98b65da54ac8
Beth Strickland <bubbly01ky@icloud.com>
2018-08-21 11:34:46 -0400

    finished update DL

--- src/main/java/com/techelevator/model/domain/User.java
+++ src/main/java/com/techelevator/model/domain/User.java
@@ -13,22 +13,11 @@ public class User {
 	})
 	private String password;
 	private String role;
-	
+	private String driversLicense;
 	private String confirmPassword;
+	
 	public String getUserName() {
 		return userName;
-	}
-	/**
-	 * @return the role
-	 */
-	public String getRole() {
-		return role;
-	}
-	/**
-	 * @param role the role to set
-	 */
-	public void setRole(String role) {
-		this.role = role;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
@@ -38,6 +27,18 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getDriversLicense() {
+		return driversLicense;
+	}
+	public void setDriversLicense(String driversLicense) {
+		this.driversLicense = driversLicense;
 	}
 	public String getConfirmPassword() {
 		return confirmPassword;
